cf8ceb16b01efa931c5e37bb11abd3fc2e0bd3b6
seungmin0904 <gogo90490@gmail.com>
2025-02-27 09:39:10 +0900

    Update ifTest6.java

--- ch3_condition/src/condition/ifTest6.java
+++ ch3_condition/src/condition/ifTest6.java
@@ -4,31 +4,43 @@ import java.util.Scanner;
 
 public class ifTest6 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("점수를 입력하세요>>");
-        String input = scan.nextLine();
+        // if (condition) {
+        //     if (condition) {
+                
+        //     } else {
+                
+        //     }
+        // } else {
+        //     if (condition) {
+                
+        //     } else {
+                
+        //     }
+        // }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("점수 입력");
+        String input = scanner.nextLine();
         int score = Integer.parseInt(input);
 
-        char grade = ' ', opt = '0';
+        char grade =' ', opt='0';
         if (score >= 90) {
             grade = 'A';
-            if (score >= 98) {
+            if(score>=98){
                 opt = '+';
-
-            } else if (score < 94) {
-                opt = '=';
-            }
-
-        } else if (score >= 80) {
-            grade = 'b';
-            if (score >= 88) {
-                opt = '+';
-            } else if (score < 84) {
+            }else if(score > 94){
                 opt = '-';
-            } else {
-                grade = 'C';
             }
-            System.out.printf("당신의 등급은 %c%c 입니다 \n", grade, opt);
+        }else if(score >= 80){
+            grade = 'B';
+            if(score>=88){
+                opt='+';
+            }else if(score > 84){
+                opt='-';
+            }
+        }else{
+            grade='C';
         }
+        System.out.printf("당신의 학점은 %c%c 입니다.",grade,opt);
     }
 }
